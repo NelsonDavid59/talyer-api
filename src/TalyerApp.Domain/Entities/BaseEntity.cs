34a3a3ba3feeb7916cbb83a348985d6c3aac074c
NelsonDavid59 <fleitasnelson59@gmail.com>
2026-09-19 21:24:03 -0300

    Refactor updated_at initialization

--- src/TalyerApp.Domain/Entities/BaseEntity.cs
+++ src/TalyerApp.Domain/Entities/BaseEntity.cs
@@ -4,12 +4,11 @@ namespace TalyerApp.Domain.Entities;
 public abstract class BaseEntity : IBaseEntity
 {
     public DateTime CreatedAt { get; protected set; }
-    public DateTime UpdatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
 
     protected BaseEntity()
     {
         CreatedAt = DateTime.UtcNow;
-        UpdatedAt = DateTime.UtcNow;
     }
 
     public void UpdateTimestamp()
